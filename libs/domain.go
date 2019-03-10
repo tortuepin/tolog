a754797cd0d397c6db97e4da2e69d943e91de9f7
tortuepin <kameyasu119@gmail.com>
2019-03-10 15:34:20 +0900

    Tolog関連の構造体を追加

--- libs/domain.go
+++ libs/domain.go
@@ -2,3 +2,24 @@ package tolog
 
 const FileType = ".md"
 const DateFormat = "060102"
+const TabSetting = 2
+const HeaderPrefix = "## "
+const TodoHeader = "## todo"
+const LogHeader = "## log"
+
+type TologItem struct {
+	Todo     []TodoItem `json:"todo"`
+	Log      []LogItem  `json:"log"`
+	Filename string     `json:"filename"`
+}
+type LogItem struct {
+	Name     string   `json:"name"`
+	Tag      []string `json:"tag"`
+	Contents []string `json:"contents"`
+}
+type TodoItem struct {
+	Title string `json:"title"`
+	Tag   string `json:"tag"`
+	Done  bool   `json:"done"`
+	Depth int    `json:"depth"`
+}
