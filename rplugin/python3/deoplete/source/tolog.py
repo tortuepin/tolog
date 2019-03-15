2688e4bfa10e4437ee736d28a532b8cc3885f161
tortuepin <kameyasu119@gmail.com>
2019-03-15 15:39:19 +0900

    WIP

--- rplugin/python3/deoplete/source/tolog.py
+++ rplugin/python3/deoplete/source/tolog.py
@@ -1,12 +1,11 @@
 from deoplete.source.base import Base
+  class Source(Base):
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.name = 'tolog'
+        self.mark = '[tolog]'
+        self.rank = 1000
+        self._count = 0
 
-        class Source(Base):
-            def __init__(self, vim):
-                super().__init__(vim)
-                self.name = 'tolog'
-                self.mark = '[tolog]'
-                self.rank = 1000
-                self._count = 0
-
-            def gather_candidates(self, context):
-                return ['tooooolgo']
+    def gather_candidates(self, context):
+        return ['tooooolgo', 'yakusugi']
